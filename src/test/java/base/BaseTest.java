19d2f96f3b81fa51b0fdd011664b7b19041cfd25
Rafael Mirzaev <tree.bree@mail.ru>
2026-06-13 13:31:39 +0300

    Fix RemoteWebDriver for Selenoid

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,6 +1,7 @@
 package base;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.WebDriverConfig;
 import helpers.Attach;
@@ -8,29 +9,39 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
 
 public class BaseTest {
 
     @BeforeAll
     static void beforeAll() {
         WebDriverConfig config = new WebDriverConfig();
-        Configuration.browser = config.getBrowser();
-        Configuration.browserVersion = config.getBrowserVersion();
-        Configuration.browserSize = config.getBrowserSize();
         Configuration.baseUrl = "https://www.agima.ru";
-        Configuration.remote = config.getSelenoidUrl();
+        Configuration.browserSize = config.getBrowserSize();
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.of(
-                "enableVNC", true,
-                "enableVideo", true
-        ));
-        Configuration.browserCapabilities = capabilities;
+        if (config.isRemote()) {
+            ChromeOptions options = new ChromeOptions();
+            options.setCapability("selenoid:options", Map.of(
+                    "enableVNC", true,
+                    "enableVideo", true
+            ));
+            try {
+                RemoteWebDriver driver = new RemoteWebDriver(
+                        new URL(config.getSelenoidUrl()), options);
+                WebDriverRunner.setWebDriver(driver);
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     @BeforeEach
